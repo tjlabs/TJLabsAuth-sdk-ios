a8986f09e95b0280920fec8143fb500774581f47
tjlabs-dev <92098727+tjlabs-dev@users.noreply.github.com>
2025-06-13 14:28:27 +0900

    feat: TJLabsAuthConstants
    update

--- TJLabsAuth/TJLabsAuthConstants.swift
+++ TJLabsAuth/TJLabsAuthConstants.swift
@@ -5,17 +5,18 @@ class TJLabsAuthConstants {
     static let TIMEOUT_VALUE_PUT: TimeInterval = 5.0
     static let TIMEOUT_VALUE_POST: TimeInterval = 5.0
     
-    static let USER_TOKEN_SERVER_VERSION = "2025-03-25"
+    static let USER_JUPITER_TOKEN_SERVER_VERSION = "2025-03-25"
+    static let USER_PHOENIX_TOKEN_SERVER_VERSION = "2025-06-11"
     
     private static let HTTP_PREFIX = "https://"
     private static let SUFFIX = ".tjlabs.dev"
-    private static let JUPITER = ".jupiter"
     
     private(set) static var REGION_PREFIX = "ap-northeast-2."
     private(set) static var REGION_NAME = "Korea"
     
     private(set) static var USER_URL = HTTP_PREFIX + REGION_PREFIX + "user"
     
+    private static var SERVER_TYPE: String = "jupiter"
     public static func setServerURL(region: String, serverType: String) {
         switch region {
         case AuthRegion.KOREA.rawValue:
@@ -32,7 +33,7 @@ class TJLabsAuthConstants {
             REGION_NAME = "Korea"
         }
         
-        USER_URL = HTTP_PREFIX + REGION_PREFIX + "user" + serverType + SUFFIX
+        USER_URL = HTTP_PREFIX + REGION_PREFIX + "user." + serverType + SUFFIX
     }
     
     public static func getUserBaseURL() -> String {
@@ -40,10 +41,22 @@ class TJLabsAuthConstants {
     }
     
     public static func getUserTokenVersion() -> String {
-        return USER_TOKEN_SERVER_VERSION
+        if SERVER_TYPE == "jupiter" {
+            return USER_JUPITER_TOKEN_SERVER_VERSION
+        } else if SERVER_TYPE == "phoenix" {
+            return USER_PHOENIX_TOKEN_SERVER_VERSION
+        } else {
+            return USER_JUPITER_TOKEN_SERVER_VERSION
+        }
     }
     
     public static func getUserTokenURL() -> String {
-        return USER_URL + "/" + USER_TOKEN_SERVER_VERSION + "/user"
+        if SERVER_TYPE == "jupiter" {
+            return USER_URL + "/" + USER_JUPITER_TOKEN_SERVER_VERSION + "/user"
+        } else if SERVER_TYPE == "phoenix" {
+            return USER_URL + "/" + USER_PHOENIX_TOKEN_SERVER_VERSION + "/user"
+        } else {
+            return USER_URL + "/" + USER_JUPITER_TOKEN_SERVER_VERSION + "/user"
+        }
     }
 }
